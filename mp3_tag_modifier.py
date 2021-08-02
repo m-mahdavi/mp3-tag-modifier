e452d1c9140a47ad2ffcb107df715319c26dd486
Mohammad <moh.mahdavi.l@gmail.com>
2021-08-02 21:48:36 +0200

    migrating to python 3

--- mp3_tag_modifier.py
+++ mp3_tag_modifier.py
@@ -3,52 +3,45 @@
 # Mohammad Mahdavi
 # moh.mahdavi.l@gmail.com
 # December 2015
-# All Rights Reserved
 ##########################################################
 
 
 ##########################################################
 import os
-import sys
 import mutagen.easyid3
 ##########################################################
 
 
 ##########################################################
-MUSIC_FOLDER_PATH = "/media/mohammad/C20E45C80E45B5E7/Temp/Music"
+MUSIC_FOLDER_PATH = "/mnt/e/Music"   # "/media/mohammad/08CC2DB1CC2D99C8/Music"   #"D:/Music"   # "/mnt/e/Music"
 ##########################################################
 
 
 ##########################################################
 def dfsUpdate(path, artist, album):
-    file_list = os.listdir(path)
-    for a_file in file_list:
-        new_path = os.path.join(path, a_file)
-        if os.path.isdir(new_path):
-            album = a_file
-            dfsUpdate(new_path, artist, album)
-        elif a_file.lower().endswith(".mp3"):
-            print new_path
-            try:
-                audio_file = mutagen.easyid3.EasyID3(new_path)
-            except:
-                audio_file = mutagen.File(new_path, easy=True)
-                audio_file.add_tags()
-            for tag in audio_file:
-                audio_file[tag] = ""
-            audio_file["artist"] = artist.decode("utf-8")
-            audio_file["album"] = album.decode("utf-8")
-            audio_file["title"] = a_file[:-4]
-            audio_file.save()
-            os.rename(new_path, new_path[:-3] + "mp3")
+	file_list = os.listdir(path)
+	for a_file in file_list:
+		new_path = os.path.join(path, a_file)
+		if os.path.isdir(new_path):
+			album = a_file
+			dfsUpdate(new_path, artist, album)
+		elif a_file.lower().endswith(".mp3"):
+			print(new_path)
+			audio_file = mutagen.easyid3.EasyID3(new_path)
+			audio_file["artist"] = artist
+			audio_file["album"] = album
+			audio_file["title"] = ""
+			# audio_file["date"] = 2000
+			audio_file.save()
 ##########################################################
 
 
 ##########################################################
 if __name__ == "__main__":
-    artist_list = os.listdir(MUSIC_FOLDER_PATH)
-    for artist in artist_list:
-        artist_folder_path = os.path.join(MUSIC_FOLDER_PATH, artist)
-        default_album = "Single"
-        dfsUpdate(artist_folder_path, artist, default_album)
+	artist_list = os.listdir(MUSIC_FOLDER_PATH)
+	for artist in artist_list:
+		artist_folder_path = os.path.join(MUSIC_FOLDER_PATH, artist)
+		if os.path.isdir(artist_folder_path):
+			default_album = "Singles"
+			dfsUpdate(artist_folder_path, artist, default_album)
 ##########################################################
